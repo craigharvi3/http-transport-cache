c2445589c6ab476c3e6ae3ea352d47302d7c62d9
nick spragg <nick.spragg@bbc.co.uk>
2017-10-25 09:22:46 +0100

    Fix: Ensure qs are used to create keys.

--- test/maxAge.js
+++ test/maxAge.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const assert = require('assert');
-const httpTransport = require('@bbc/http-transport');
+const promisifyAll = require('bluebird').promisifyAll;
 const Catbox = require('catbox');
 const Memory = require('catbox-memory');
 const nock = require('nock');
-const bluebird = require('bluebird');
+
+const httpTransport = require('@bbc/http-transport');
 
 const cache = require('../');
 
@@ -17,31 +18,47 @@ const defaultHeaders = {
 
 const defaultResponse = {
   body: 'I am a string!',
-  url: 'http://www.example.com/',
+  url: 'http://www.example.com/path',
   statusCode: 200,
   elapsedTime: 40,
   headers: defaultHeaders
 };
 
-const bodySegment = {
+const alternateResponse = {
+  body: 'clade',
+  url: 'http://www.example.com/path',
+  statusCode: 200,
+  elapsedTime: 40,
+  headers: defaultHeaders
+};
+
+const defaultBodySegment = {
   segment: 'http-transport:1.0.0:body',
-  id: 'http://www.example.com/'
+  id: 'http://www.example.com/path'
+};
+
+const alternateBodySegment = {
+  segment: 'http-transport:1.0.0:body',
+  id: 'http://www.example.com/path?a=1'
 };
 
 nock.disableNetConnect();
 
 function createCache() {
   const cache = new Catbox.Client(new Memory());
-  bluebird.promisifyAll(cache);
+  promisifyAll(cache);
 
   return cache;
 }
 
-function requestWithCache(catbox) {
+function requestWithCache(catbox, url, qs) {
+  if (!url) url = 'http://www.example.com/path';
+
   return httpTransport
     .createClient()
     .use(cache.maxAge(catbox))
-    .get('http://www.example.com/')
+    .query(qs)
+    .get(url)
     .asResponse();
 }
 
@@ -59,12 +76,12 @@ describe('Max-Age', () => {
 
   it('stores cached values for the max-age value', () => {
     const cache = createCache();
-    api.get('/').reply(200, defaultResponse.body, defaultHeaders);
+    api.get('/path').reply(200, defaultResponse.body, defaultHeaders);
 
     const expiry = Date.now() + 60000;
 
     return requestWithCache(cache)
-      .then(() => cache.getAsync(bodySegment))
+      .then(() => cache.getAsync(defaultBodySegment))
       .then(cached => {
         const actualExpiry = cached.ttl + cached.stored;
         const differenceInExpires = actualExpiry - expiry;
@@ -74,26 +91,48 @@ describe('Max-Age', () => {
       });
   });
 
+  it('varies on query strings', () => {
+    const cache = createCache();
+    api.get('/path').reply(200, defaultResponse.body, defaultHeaders);
+    api.get('/path?a=1').reply(200, alternateResponse.body, defaultHeaders);
+
+    const pending = [];
+    pending.push(requestWithCache(cache));
+    pending.push(requestWithCache(cache, alternateResponse.url, { a: 1 }));
+
+    return Promise.all(pending)
+      .then(() => {
+        const cached = [];
+        cached.push(cache.getAsync(defaultBodySegment));
+        cached.push(cache.getAsync(alternateBodySegment));
+        return Promise.all(cached);
+      })
+      .then(results => {
+        assert.equal(results[0].item.body, defaultResponse.body);
+        assert.equal(results[1].item.body, alternateResponse.body);
+      });
+  });
+
   it('does not store if no cache-control', () => {
     const cache = createCache();
-    api.get('/').reply(200, defaultResponse);
+    api.get('/path').reply(200, defaultResponse);
 
     return requestWithCache(cache)
-      .then(() => cache.getAsync(bodySegment))
+      .then(() => cache.getAsync(defaultBodySegment))
       .then(cached => assert(!cached));
   });
 
   it('does not store if max-age=0', () => {
     const cache = createCache();
 
-    api.get('/').reply(200, defaultResponse, {
+    api.get('/path').reply(200, defaultResponse, {
       headers: {
         'cache-control': 'max-age=0'
       }
     });
 
     return requestWithCache(cache)
-      .then(() => cache.getAsync(bodySegment))
+      .then(() => cache.getAsync(defaultBodySegment))
       .then(cached => assert(!cached));
   });
 
@@ -106,18 +145,18 @@ describe('Max-Age', () => {
       body: 'http-transport',
       headers,
       statusCode: 200,
-      url: 'http://www.example.com/',
+      url: 'http://www.example.com/path',
       elapsedTime: 40
     };
 
     const cache = createCache();
-    api.get('/').reply(200, defaultResponse, {
+    api.get('/path').reply(200, defaultResponse, {
       headers
     });
 
     return cache
       .startAsync()
-      .then(() => cache.setAsync(bodySegment, cachedResponse, 600))
+      .then(() => cache.setAsync(defaultBodySegment, cachedResponse, 600))
       .then(() => requestWithCache(cache))
       .then(res => {
         assert.equal(res.body, cachedResponse.body);
@@ -126,7 +165,7 @@ describe('Max-Age', () => {
         assert.equal(res.url, cachedResponse.url);
         assert.equal(res.elapsedTime, cachedResponse.elapsedTime);
 
-        return cache.drop(bodySegment);
+        return cache.drop(defaultBodySegment);
       });
   });
 });
